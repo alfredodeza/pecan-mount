8fdf64f3fa651315d26228fff4d1802b222f9785
Alfredo Deza <alfredodeza@gmail.com>
2013-02-03 13:40:08 -0500

    update tests to reflect the root arg removed

--- pecan_mount/tests/unit/test_mount.py
+++ pecan_mount/tests/unit/test_mount.py
@@ -17,22 +17,22 @@ class TestTreeMount(object):
         assert pecan_mount.tree.apps == {}
 
     def test_add_app_to_tree(self):
-        pecan_mount.tree.mount(None, '', self.conf)
+        pecan_mount.tree.mount( '', self.conf)
         assert pecan_mount.tree.apps[''] is not None
 
     def test_add_app_to_tree_has_script_name(self):
-        pecan_mount.tree.mount(None, '', self.conf)
+        pecan_mount.tree.mount( '', self.conf)
         assert pecan_mount.tree.apps[''].script_name == ''
 
     def test_fail_to_add_two_apps_in_same_location(self):
-        pecan_mount.tree.mount(None, '', self.conf)
+        pecan_mount.tree.mount( '', self.conf)
         with raises(AttributeError) as exc:
-            pecan_mount.tree.mount(None, '', self.conf)
+            pecan_mount.tree.mount( '', self.conf)
         assert "script_name <''" in exc.value[0]
         assert 'is already mounted' in exc.value[0]
 
     def test_strip_trailing_slash(self):
-        pecan_mount.tree.mount(None, '/foo/', self.conf)
+        pecan_mount.tree.mount( '/foo/', self.conf)
         assert pecan_mount.tree.apps['/foo']
         assert pecan_mount.tree.apps.get('/foo/') is None
 
@@ -52,14 +52,14 @@ class TestTreeScriptName(object):
         assert pecan_mount.tree.script_name('/') is None
 
     def test_return_path(self):
-        pecan_mount.tree.mount(None, '/foo', self.conf)
+        pecan_mount.tree.mount( '/foo', self.conf)
         assert pecan_mount.tree.script_name('/foo') == '/foo'
 
     def test_return_nested_path(self):
-        pecan_mount.tree.mount(None, '/foo', self.conf)
+        pecan_mount.tree.mount( '/foo', self.conf)
         assert pecan_mount.tree.script_name('/foo/bar/baz') == '/foo'
 
     def test_return_none_if_path_is_empty(self):
         # if is emtpy and is not in self.apps
-        pecan_mount.tree.mount(None, '/foo', self.conf)
+        pecan_mount.tree.mount( '/foo', self.conf)
         assert pecan_mount.tree.script_name('') == None
