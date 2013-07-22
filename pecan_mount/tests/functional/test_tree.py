264a7b377818a7ca7bf270032d031cf6692f2b69
Alfredo Deza <alfredodeza@gmail.com>
2013-07-22 19:50:42 -0400

    renaming TestApp so it doesn't confuse the test runner

--- pecan_mount/tests/functional/test_tree.py
+++ pecan_mount/tests/functional/test_tree.py
@@ -1,4 +1,4 @@
-from webtest import TestApp
+from webtest import TestApp as WebTestApp
 import pecan_mount
 
 
@@ -16,21 +16,21 @@ class TestTwoApps(object):
     def test_not_found_if_root_is_empty(self):
         pecan_mount.tree.mount('/foo', self.conf)
         pecan_mount.tree.mount('/bar', self.conf)
-        app = TestApp(pecan_mount.tree)
+        app = WebTestApp(pecan_mount.tree)
         result =  app.get('/', expect_errors=True)
         assert result.status_int == 404
 
     def test_run_two_apps(self):
         pecan_mount.tree.mount('/foo', self.conf)
         pecan_mount.tree.mount('/bar', self.conf)
-        app = TestApp(pecan_mount.tree)
+        app = WebTestApp(pecan_mount.tree)
         assert app.get('/bar/')
         assert app.get('/foo/')
 
     def test_run_two_nested_apps(self):
         pecan_mount.tree.mount('/foo/baz', self.conf)
         pecan_mount.tree.mount('/bar/foo/foo', self.conf)
-        app = TestApp(pecan_mount.tree)
+        app = WebTestApp(pecan_mount.tree)
         assert app.get('/bar/foo/foo')
         assert  app.get('/foo/baz')
 
